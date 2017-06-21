3684c82fd7529d7c6ffe3d3cce20a4722494122d
Gurpreet Singh <gurpreet@u.nus.edu>
2017-06-21 12:00:12 -0400

    Delete know_slices.m

--- know_slices.m
+++
@@ -1,9 +0,0 @@
-function [slices]=know_slices(List)
-lof=length(List);
-for i=1:lof
-patnumber=List(i,1);
-vidnumber=List(i,2);
-filename=sprintf('/data/Gurpreet/Echo/%d/%d(%d).dcm',patnumber,patnumber,vidnumber);
-dfile=size(dicomread(filename));
-slices(i,1)=dfile(1,4);
-end
