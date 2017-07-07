782cc9995f5c47975dbfd3d5b5bde26f7a85b10f
gtm2122 <gtm2122@github.com>
2017-07-07 15:16:12 -0400

    commiting

--- view_testls/Train_Resnet_Sets.py
+++ view_testls/Train_Resnet_Sets.py
@@ -26,24 +26,25 @@ def Train_Resnet_Sets(gpunum,mainfolder,number_of_sets,number_of_views,lr,lr_dec
     from Conmat_TPR import calculate_APR as CAPR
     from IPython.display import clear_output
     import numpy as np
+    
     #-------------------------------------------------------------------------------------------------------#
     temp_Pr=np.zeros((number_of_views,1))
     temp_Re=np.zeros((number_of_views,1))
     Total_Accuracy=np.zeros((number_of_sets,1))
-    for j in xrange(0,number_of_sets):
+    for j in range(0,number_of_sets):
         k=j+1
         setnumber="SET"+str(k)
-        print "============================================================================================"
-        print "=============================="+setnumber+"=========================================================="
+        print ("============================================================================================")
+        print ("=============================="+setnumber+"==========================================================")
         Model_name="ResNetModel_Pretrained_"+str(number_of_views)+"_views_bs_"+str(batch_size)+"_e_"+str(epochs)+"_"+str(Identifier)+".pth.tar"
         datapath=mainfolder+setnumber+"/dataset/"
         savepath=mainfolder+setnumber+"/"+Model_name
         res = models.resnet18(pretrained=True)
         res.fc = nn.Linear(res.fc.in_features,number_of_views)
-        obj = model_pip(model_in=res,scale=True,batch_size=batch_size,use_gpu=True,gpu=gpunum,data_path=datapath,lr=lr,lr_decay_epoch=lr_decay)
+        obj = model_pip(model_in=res,scale=True,batch_size=batch_size,use_gpu=True,gpu=gpunum,data_path=datapath,lr=lr,lr_decay_epoch=lr_decay,verbose=True)
         model = obj.train_model(epochs=epochs)
         obj.store_model(f_name=savepath)
-        print "==============================Testing Started==============================================="
+        print ("==============================Testing Started===============================================")
         mdir=mainfolder+str(setnumber)+"/"
         obj.test(model_dir=mdir,model_name=Model_name,save_miscl = True,test_on=True,n='None',random_crop=False,folder='misc')
         del (obj)
@@ -54,8 +55,8 @@ def Train_Resnet_Sets(gpunum,mainfolder,number_of_sets,number_of_views,lr,lr_dec
         temp_Pr=np.concatenate((temp_Pr,Pr),axis=1)
         temp_Re=np.concatenate((temp_Re,Re),axis=1)
         Total_Accuracy[j,0]=TA
-        print temp_Pr.shape
-        print Pr.shape
+        print (temp_Pr.shape)
+        print (Pr.shape)
         save_file=mainfolder+setnumber+"/Confusion_matrix"+Identifier+".txt"
         np.savetxt(save_file,Confusion_mat)
 
@@ -72,7 +73,7 @@ def Train_Resnet_Sets(gpunum,mainfolder,number_of_sets,number_of_views,lr,lr_dec
     Total_Accuracy=np.nan_to_num(Total_Accuracy)
     Precision=np.nan_to_num(Precision)
     Recall=np.nan_to_num(Recall)
-    print Total_Accuracy
+    print (Total_Accuracy)
     Total_Accuracy_mean=np.average(Total_Accuracy,axis=0)
     Precision_mean=np.average(Precision,axis=1)
     Recall_mean=np.average(Recall,axis=1)
