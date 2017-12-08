613838880bca197b8c936b4afbf2ce462157fa6e
Ubuntu <ubuntu@ip-172-31-2-181.us-west-2.compute.internal>
2017-12-08 18:24:28 +0000

    multi gpu

--- view_testls/LSTM_view/arrange_bnecks.py
+++ view_testls/LSTM_view/arrange_bnecks.py
@@ -11,20 +11,25 @@ def arrange_data(d):
 	dirs = os.listdir(d)
 	for i in dirs:
 		#try:
+		#print(i)
 		if(os.path.exists(d+'/'+i)):
+			#print('dere')
 			if i[:find_2(i)] not in unique and '.jpg.pth' in i:
 				unique.append(i[:find_2(i)+1])
 				if(not(os.path.exists(d+'/'+unique[-1]))):
 					os.makedirs(d+'/'+unique[-1])
+					print(d+'/'+unique[-1])
 				for k,j in enumerate(dirs):
 					if unique[-1] in j[:len(unique[-1])]:
 						print(d+'/'+j,d+'/'+unique[-1]+'/')
-						shutil.move(d+'/'+j,d+'/'+unique[-1]+'/')
+						if('.' in d+'/'+j):
+							shutil.copy(d+'/'+j,d+'/'+unique[-1]+'/')
 		# except:
 		# 	continue
 
 
 for phase in ['train','test','val']:
 	for c in os.listdir('/storage/SET2_bnecks/'+phase):
-		if(len(c)==3):
+		if('.' not in c):
 			arrange_data('/storage/SET2_bnecks/'+phase+'/'+c)
+			print('/storage/SET2_bnecks/'+phase+'/'+c)
