37673daf882c841acf8579d755a496a6f63d6610
gtm2122 <gabrielthomas281@gmail.com>
2018-02-27 09:11:45 -0800

    fixed bugs

--- view_testls/LSTM_view/rescale_f.py
+++ view_testls/LSTM_view/rescale_f.py
@@ -2,6 +2,7 @@
 import torch
 import os
 import shutil
+import numpy as np
 def rescale_f(src,des):
 	### path is a directory of folder containing features stored as .pth
 	try:
@@ -11,14 +12,9 @@ def rescale_f(src,des):
 		os.makedirs(des)
 	for img in os.listdir(src):
 		out_save = torch.load(src+'/'+img)
-		min_values,_ = out_save.min(dim=1)
-		max_values,_ = out_save.max(dim=1)
-		
-		min_values = min_values.unsqueeze(1)
-		max_values = max_values.unsqueeze(1)
-		
+		min_values = np.min(out_save)
+		max_values = np.max(out_save)
 		out_saveN = -1 + 2*(out_save-min_values)/(max_values-min_values)
-
 		torch.save(out_saveN,des+'/'+img)
 
 
@@ -29,24 +25,27 @@ def rescale_wrapper(main_dir,des_dir):
 	except:
 		shutil.rmtree(des_dir)
 		os.makedirs(des_dir)
-
+	#print(des_dir+'/'+'o')
 	for i in os.listdir(main_dir):
+		print(i)
 		if('test' in i or 'train' in i or 'val' in i):
 			if not(os.path.isdir(des_dir+'/'+i)):
-				os.makedirs(os.makedirs(des_dir+'/'+i))
-
+				os.makedirs(des_dir+'/'+i)
+				
 			for j in os.listdir(main_dir+'/'+i):
 				### inside main_dir/train for eg
 				if ('.p' not in j):
 					class_path = main_dir+'/'+i+'/'+j
 
 					if not(os.path.isdir(des_dir+'/'+i+'/'+j)):
-						os.makedirs(os.makedirs(des_dir+'/'+i+'/'+j))
+						os.makedirs(des_dir+'/'+i+'/'+j)
 						
 					for k in os.listdir(class_path):
 						if(os.path.isdir(class_path+'/'+k)):
 							if not(os.path.isdir(des_dir+'/'+i+'/'+j+'/'+k)):
-								os.makedirs(os.makedirs(des_dir+'/'+i+'/'+j+'/'+k))
+								os.makedirs(des_dir+'/'+i+'/'+j+'/'+k)
 							print(class_path+'/'+k,des_dir+'/'+i+'/'+j+'/'+k)
-							rescale(class_path+'/'+k,des_dir+'/'+i+'/'+j+'/'+k)
+							rescale_f(class_path+'/'+k,des_dir+'/'+i+'/'+j+'/'+k)
 
+
+rescale_wrapper(main_dir='/data/gabriel/SET1_bnecks/',des_dir='/data/gabriel/SET1_bnecks_normed/')
