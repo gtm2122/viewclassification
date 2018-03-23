3c68801711f79c0f2a81406cbe5bd069561ef768
gtm2122 <gabrielthomas281@gmail.com>
2018-03-23 07:21:17 -0700

    modified arrangement function to cater to images

--- view_testls/LSTM_view/arrange_bnecks.py
+++ view_testls/LSTM_view/arrange_bnecks.py
@@ -14,7 +14,29 @@ def arrange_data(d):
 		#print(i)
 		if(os.path.exists(d+'/'+i)):
 			#print('dere')
-			if i[:find_2(i)] not in unique and '.jpg.pth' in i:
+			if i[:find_2(i)] not in unique and '.jpg.pth' in i or '.JPEG.pth' in i:
+				unique.append(i[:find_2(i)+1])
+				if(not(os.path.exists(d+'/'+unique[-1]))):
+					os.makedirs(d+'/'+unique[-1])
+					print(d+'/'+unique[-1])
+				for k,j in enumerate(dirs):
+					if unique[-1] in j[:len(unique[-1])]:
+						print(d+'/'+j,d+'/'+unique[-1]+'/')
+						if('.' in d+'/'+j):
+							shutil.copy(d+'/'+j,d+'/'+unique[-1]+'/')
+		# except:
+		# 	continue
+
+def arrange_data_img(d):
+	unique=[]
+	dirs = os.listdir(d)
+	for i in dirs:
+		#try:
+		#print(i)
+		if(os.path.exists(d+'/'+i)):
+			#print('dere')
+			#print(d+'/'+i)
+			if i[:find_2(i)] not in unique and '.jpg' in i or '.png' in i:
 				unique.append(i[:find_2(i)+1])
 				if(not(os.path.exists(d+'/'+unique[-1]))):
 					os.makedirs(d+'/'+unique[-1])
@@ -28,8 +50,18 @@ def arrange_data(d):
 		# 	continue
 
 
-for phase in ['train','test','val']:
-	for c in os.listdir('/storage/SET2_bnecks/'+phase):
+#for phase in ['train','test','val']:
+
+"""
+for phase in ['test_distort_2_16','train_distort_2_16','val_distort_2_16']:
+	for c in os.listdir('/data/gabriel/SET1_bnecks_2_16/'+phase):
 		if('.' not in c):
-			arrange_data('/storage/SET2_bnecks/'+phase+'/'+c)
-			print('/storage/SET2_bnecks/'+phase+'/'+c)
+			arrange_data('/data/gabriel/SET1_bnecks_2_16/'+phase+'/'+c)
+			print('/data/gabriel/SET1_bnecks_2_16/'+phase+'/'+c)
+
+"""
+for phase in ['train','val','test']:
+	for c in os.listdir('/data/gabriel/dataset/'+phase):
+		if('.' not in c):
+			arrange_data_img('/data/gabriel/dataset/'+phase+'/'+c)
+			print('/data/gabriel/dataset/'+phase+'/'+c)
